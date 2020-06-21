debb36494063112c637ac490b627fe21a7a8e0e4
d0rianb <dorian.beauchesne@icloud.com>
2020-06-21 13:56:10 +0200

    add 'all' argument to Logger.clear()

---
+++ test/main.test.js
@@ -0,0 +1,75 @@
+const fs = require('fs')
+const Logger = require('../lib/main.js')
+
+const logsDir = './logs'
+const defaultFile = `logs.log`
+
+describe('Logs method in default file', () => {
+    afterEach(() => Logger.clear())
+
+    it('Should info in default file', () => {
+        const log = 'test info in default file'
+        Logger.info(log)
+        const content = fs.readFileSync(`${logsDir}/${defaultFile}`, { encoding: 'utf8' })
+        expect(content).toContain(log)
+    })
+
+    it('Should debug in default file', () => {
+        const log = 'test debug in default file'
+        Logger.debug(log)
+        const content = fs.readFileSync(`${logsDir}/${defaultFile}`, { encoding: 'utf8' })
+        expect(content).toContain(log)
+    })
+
+    it('Should warn in default file', () => {
+        const log = 'test warn in default file'
+        Logger.warn(log)
+        const content = fs.readFileSync(`${logsDir}/${defaultFile}`, { encoding: 'utf8' })
+        expect(content).toContain(log)
+    })
+
+    it('Should error in default file', () => {
+        const log = 'test error in default file'
+        Logger.error(log)
+        const content = fs.readFileSync(`${logsDir}/${defaultFile}`, { encoding: 'utf8' })
+        expect(content).toContain(log)
+    })
+
+    it('Should fatal in default file', () => {
+        const log = 'test fatal in default file'
+        Logger.fatal(log)
+        const content = fs.readFileSync(`${logsDir}/${defaultFile}`, { encoding: 'utf8' })
+        expect(content).toContain(log)
+    })
+
+})
+
+describe('Options modification', () => {
+    afterEach(() => Logger.clear('test.log'))
+    it('Should log INFO in test.log via call', () => {
+        Logger.info('INFO', 'test.log')
+        const content = fs.readFileSync(`${logsDir}/test.log`, { encoding: 'utf8' })
+        expect(content).toContain('INFO')
+    })
+
+    it('Should log INFO in test.log via options', () => {
+        Logger.setOptions({ filename: 'test.log' })
+        Logger.info('INFO')
+        const content = fs.readFileSync(`${logsDir}/test.log`, { encoding: 'utf8' })
+        expect(content).toContain('INFO')
+    })
+})
+
+describe('Level modification', () => {
+    it('should add a new levels', () => {
+        const nbLevels = Object.keys(Logger.levels).length
+        Logger.addLevel('Test')
+        const nbNewLevels = Object.keys(Logger.levels).length
+        expect(nbNewLevels).toBeGreaterThan(nbLevels)
+    })
+
+    it('should return the TEST level', () => {
+        const testLevel = Logger.getLevel('Test')
+        expect(testLevel).toContain('TEST')
+    })
+})
--- test/test.js
+++ test/test.js
@@ -1,34 +1,5 @@
 const Logger = require('../lib/main.js')
 
 
-// Logger
-//     .info('test d\'info', 'info.log')
-//     .debug('test de debug', 'debug.log')
-//     .warn('test de warn', 'warn.log')
-//     .error('test d\'erreur', 'error.log')
-//     .fatal('test de fatal', 'fatal.log')
-//     .info('test de défaut')
-//
-// Logger.options = { filename: 'newLogs.log' }
-// Logger.info('test de défaut dans Newlog.log')
-// Logger.clear()
-// Logger.options = { displayLevel: 3 }
-// Logger.addLevel('ULTIMATE')
-
-
-function performanceTest() {
-    const start = Date.now()
-    Logger.setOptions({ useMoment: false })
-    for (let i = 0; i < 10000; i++) {
-        Logger.info(`test n°${i}`)
-    }
-    console.log(`${Date.now() - start} ms`) // 75ms --> 10ms
-
-}
-
-function run() {
-    Logger.clear()
-    performanceTest()
-}
-
-run()
+Logger.info('test info')
+Logger.clear('all')
