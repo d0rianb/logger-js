57b83c1beff99f1af3c9b65b56755c33caa9b915
d0rianb <dorian.beauchesne@icloud.com>
2020-07-05 10:41:22 +0200

    Add speed-date - performance gain

--- test/test.js
+++ test/test.js
@@ -1,7 +1,12 @@
 const Logger = require('../lib/main.js')
 
 
-Logger.setOptions({ filename: 'test.log' })
-Logger.info('test info', 'test.log')
-Logger.warn('test info')
-// Logger.clear('all')
+Logger.setOptions({ filename: 'perf.log' })
+Logger.clear()
+
+let start = Date.now()
+for (let i = 0; i < 10000; i++) {
+    Logger.info(`info n°${i}`)
+}
+
+console.log(`time : ${Date.now() - start}ms`)
